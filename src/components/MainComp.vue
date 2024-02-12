daa03fc7ddf8f22e0a20fd8f512e783625d4b397
MicheleIanni86 <micheleianni86@gmail.com>
2024-02-12 19:47:26 +0100

    feat: Add link with Moking Api in MainComponents and add Link API with Endpoint from GlobalState

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,119 +1,24 @@
 <script>
-import { store } from '../store'
+import { store } from '../store';
+import axios from 'axios';
 import CardComp from './CardComp.vue';
 export default {
   data() {
     return {
     products: [
-        {
-            id: 1,
-            frontImage: './src/assets/img/1.webp',
-            backImage: './src//assets/img/1b.webp',
-            brand: "Levi's",
-            name: 'Relaxed Fit',
-            price: 29.99,
-            isInFavorites: true,
-            badges: [
-            {
-                type: 'tag',
-                value: 'Sostenibilità',
-            },
-            {
-                type: 'discount',
-                value: '-50%',
-            },
-            ],
-        },
-        {
-            id: 2,
-            frontImage: './src/assets/img/2.webp',
-            backImage: './src/assets/img/2b.webp',
-            brand: 'Guess',
-            name: 'Roses Tee',
-            price: 20.99,
-            isInFavorites: true,
-            badges: [
-            {
-                type: 'discount',
-                value: '-30%',
-            },
-            ],
-        },
-        {
-            id: 3,
-            frontImage: './src/assets/img/3.webp',
-            backImage: './src/assets/img/3b.webp',
-            brand: 'Come Zucchero Filato',
-            name: 'Voglia di colori pastello',
-            price: 129.99,
-            isInFavorites: false,
-            badges: [
-            {
-                type: 'discount',
-                value: '-30%',
-            },
-            ],
-        },
-        {
-            id: 4,
-            frontImage: './src/assets/img/4.webp',
-            backImage: './src/assets/img/4b.webp',
-            brand: "Levi's",
-            name: 'Tee Unisex',
-            price: 14.99,
-            isInFavorites: false,
-            badges: [
-            {
-                type: 'tag',
-                value: 'Sostenibilità',
-            },
-            {
-                type: 'discount',
-                value: '-50%',
-            },
-            ],
-        },
-        {
-            id: 5,
-            frontImage: './src/assets/img/5.webp',
-            backImage: './src/assets/img/5b.webp',
-            brand: 'Maya Deluxe',
-            name: 'Stripe Bodice',
-            price: 99.99,
-            isInFavorites: true,
-            badges: [
-            {
-                type: 'tag',
-                value: 'Sostenibilità',
-            },
-            {
-                type: 'discount',
-                value: '-50%',
-            },
-            ],
-        },
-        {
-            id: 6,
-            frontImage: './src/assets/img/6.webp',
-            backImage: './src/assets/img/6b.webp',
-            brand: 'Esprit',
-            name: 'Maglione - Black',
-            price: 29.99,
-            isInFavorites: true,
-            badges: [
-            {
-                type: 'tag',
-                value: 'Sostenibilità',
-            },
-            ],
-        }
     ],
 
   
 };
 },
 
-components: { CardComp }
+components: { CardComp },
+
+created() {
+    axios.get(`${store.apiBoolando}/products`).then((risposta) =>{
+        this.products = risposta.data;
+    });
+ }, 
 };
 
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,5 +2,5 @@ import { reactive } from 'vue';
 
 export const store = reactive({
     //qui andranno i data GLOBALI
-
+    apiBoolando: 'http://localhost:3000',
 });
